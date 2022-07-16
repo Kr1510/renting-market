bd0b89a86501884c8d31ea13e0e0cd575dbd8b86
kr1510 <kristenhide@gmail.com>
2022-07-16 20:27:35 +0200

    Initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import CarsView from '@/views/CarsView.vue'
+import OfficesView from '@/views/OfficesView.vue'
+import BookingsView from '@/views/BookingsView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/cars',
+      name: 'cars',
+      component: CarsView
+    },
+    {
+      path: '/offices',
+      name: 'offices',
+      component: OfficesView
+    },
+    {
+      path: '/bookings',
+      name: 'bookings',
+      component: BookingsView
+    }
+  ]
+})
+
+export default router
